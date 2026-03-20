17a775a8b84818756d0686b56e52a186606266e2
cuong912005~ <nguyenthaccuong91@gmail.com>
2026-03-20 07:59:00 +0700

    Week4: deploy render

--- Week4/Code_demo/app.py
+++ Week4/Code_demo/app.py
@@ -161,4 +161,5 @@ if __name__ == "__main__":
     print("  Swagger UI    : http://localhost:5000/api/docs")
     print("  OpenAPI spec  : http://localhost:5000/api/spec")
     print("=" * 55)
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
